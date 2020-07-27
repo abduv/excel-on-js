22018aa734f2633683d4389b516b1861ee5fc710
Abdurrahman Syzdykov <syzdykovabdurrahman@gmail.com>
2020-07-28 03:36:20 +0600

    commit for 8 lesson of chapter 3

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -12,6 +12,8 @@ export class Excel {
         this.components = this.components.map(Component => {
             const $el = $.create('div', Component.className)
             const component = new Component($el)
+            // DEBUG
+            window['c' + component.name] = component
             $el.html(component.toHTML())
             $root.append($el)
             return component
@@ -24,5 +26,6 @@ export class Excel {
         this.$el.append(this.getRoot())
 
         this.components.forEach(component => component.init())
+        // window.onunload = this.components.forEach(component => component.destroy())
     }
 }
--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -6,7 +6,7 @@ export class Formula extends ExcelComponent {
     constructor($root) {
         super($root, {
             name: 'Formula',
-            listeners: ['input', 'click']
+            listeners: ['input']
         })
     }
 
@@ -18,6 +18,6 @@ export class Formula extends ExcelComponent {
     }
 
     onInput(event) {
-        console.log('Formula onInput', event, this)
+        console.log('Formula onInput', event)
     }
 }
