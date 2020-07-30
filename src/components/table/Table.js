62d5a300dfbd0686877e4c7888e2cb32224f9512
Abdurrahman Syzdykov <syzdykovabdurrahman@gmail.com>
2020-07-31 03:01:55 +0600

    Finish table resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,7 @@
 import {ExcelComponent} from '@core/ExcelComponent'
 import {createTable} from '@/components/table/table.template';
-import {$} from '@core/dom';
+import {resizeHandler} from '@/components/table/resizeLogic';
+import {shouldResize} from '@/components/table/table.functions';
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
@@ -17,52 +18,8 @@ export class Table extends ExcelComponent {
     }
 
     onMousedown(e) {
-        const resizeType = e.target.dataset.resize
-        if (resizeType) {
-            const $resizer = $(e.target)
-            const $parent = $resizer.closest('[data-type="resizable"]')
-            const coords = $parent.getCoords()
-            const index = $parent.dataset.col || null
-            const sideProp = resizeType === 'col' ? 'bottom' : 'right'
-            let value
-
-            $resizer.css({
-                opacity: 1,
-                [sideProp]: '-2000px'
-            })
-
-            document.onmousemove = (event) => {
-                if (resizeType === 'col') {
-                    const delta = event.clientX - coords.right
-                    value = coords.width + delta
-                    $resizer.css({
-                        right: -delta + 'px'
-                    })
-                } else {
-                    const delta = event.clientY - coords.bottom
-                    value = coords.height + delta
-                    $resizer.css({
-                        bottom: -delta + 'px'
-                    })
-                }
-            }
-
-            document.onmouseup = () => {
-                document.onmousemove = null
-                document.onmouseup = null
-
-                if (resizeType === 'col') {
-                    const cells = this.$root.findAll(`[data-col="${index}"]`)
-                    $parent.css({width: value + 'px'})
-                    cells.forEach(el => {
-                        el.style.width = value + 'px'
-                    })
-                } else {
-                    $parent.css({height: value + 'px'})
-                }
-
-                $resizer.removeCSS('opacity', 'bottom', 'right')
-            }
+        if (shouldResize(e)) {
+            resizeHandler(e, this.$root)
         }
     }
 }
---
+++ src/components/table/resizeLogic.js
@@ -0,0 +1,52 @@
+import {$} from '@core/dom';
+
+
+export function resizeHandler(e, $root) {
+    const $resizer = $(e.target)
+    const $parent = $resizer.closest('[data-type="resizable"]')
+    const resizeType = $resizer.dataset.resize
+    const coords = $parent.getCoords()
+    const index = $parent.dataset.col || null
+    const sideProp = resizeType === 'col' ? 'bottom' : 'right'
+    let value
+
+    $resizer.css({
+        opacity: 1,
+        [sideProp]: '-5000px'
+    })
+
+    document.onmousemove = (event) => {
+        if (resizeType === 'col') {
+            const delta = event.clientX - coords.right
+            value = coords.width + delta
+            $resizer.css({
+                right: -delta + 'px'
+            })
+        } else {
+            const delta = event.clientY - coords.bottom
+            value = coords.height + delta
+            $resizer.css({
+                bottom: -delta + 'px'
+            })
+        }
+    }
+
+    document.onmouseup = () => {
+        if (resizeType === 'col') {
+            const cells = $root.findAll(`[data-col="${index}"]`)
+            $parent.css({
+                width: value + 'px'
+            })
+            cells.forEach(el => el.style.width = value + 'px')
+        } else {
+            $parent.css({
+                height: value + 'px'
+            })
+        }
+
+        $resizer.removeCSS('opacity', 'bottom', 'right')
+
+        document.onmousemove = null
+        document.onmouseup = null
+    }
+}
